6ca2cf466e014fb19b01eb8aec89f54c26b09d0a
Stuart Olivera <stuart@stuartolivera.com>
2017-05-24 12:27:03 -0700

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/index'
+
+  root 'pages#index'
 end
