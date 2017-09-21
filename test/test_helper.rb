569fc1ed0f4c9335aa7a45bd8e5cf2aa7c43ec8c
Stuart Olivera <stuart@stuartolivera.com>
2017-09-21 19:38:19 -0400

    Report coverage on Travis CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,16 @@
+if ["manual", "travis"].include?(ENV["RUN_COVERAGE"])
+  require 'simplecov'
+  require 'codeclimate-test-reporter' if ENV["RUN_COVERAGE"] == "travis"
+  SimpleCov.add_filter 'vendor/'
+  SimpleCov.add_filter 'app/mailers/mail_preview.rb'
+  if ENV["RUN_COVERAGE"] == "travis"
+    SimpleCov.formatters = []
+    SimpleCov.start CodeClimate::TestReporter.configuration.profile
+  else
+    SimpleCov.start 'rails'
+  end
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
