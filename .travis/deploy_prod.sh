7288ad81d1be98b038805bee4598f5d158622f4c
Stuart Olivera <stuart@stuartolivera.com>
2017-09-21 23:07:25 -0400

    Copy automated deployment with Travis from BrickHack

--- .travis/deploy_prod.sh
+++ .travis/deploy_prod.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)" # start the ssh agent
-openssl aes-256-cbc -K $encrypted_60565ecba2d1_key -iv $encrypted_60565ecba2d1_iv -in .travis/deploy_ssh_key.enc -out .travis/deploy_ssh_key -d
+openssl aes-256-cbc -K $encrypted_493a21d0641d_key -iv $encrypted_493a21d0641d_iv -in .travis/deploy_ssh_key.enc -out .travis/deploy_ssh_key -d
 chmod 600 .travis/deploy_ssh_key # this key should have push access
 ssh-add .travis/deploy_ssh_key
 git remote add deploy dokku@wic.oweb.co:wichacks-prod
