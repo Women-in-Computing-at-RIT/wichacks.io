6cae3a645d9e8d97490338f334949b5c6b87bec7
Stuart Olivera <stuart@stuartolivera.com>
2018-02-01 18:15:55 -0500

    Don't throw exception upon invalid tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
+
+  private
+
+  def invalid_token
+    flash[:notice] = "Session expired"
+    redirect_to root_url
+  end
 end
