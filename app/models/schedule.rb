a08903d18b9aa22fc9568dfc10a35d76e8b507cc
Stuart Olivera <stuart@stuartolivera.com>
2019-03-02 15:29:25 -0500

    Refactor schedule from brickhack

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,35 +1,24 @@
 require 'httparty'
 SHEETS_KEY = ENV['GOOGLESHEETS_KEY']
-SHEETS_URL = "https://sheets.googleapis.com/v4/spreadsheets/".freeze
+SHEETS_URL = "https://sheets.googleapis.com/v4/spreadsheets".freeze
 SHEETS_FIELDS = "fields=sheets(data.rowData.values.userEnteredValue)".freeze
 SECTION = ":section".freeze
 ITEM = ":item".freeze
 
-# https://github.com/codeRIT/brickhack.io/wiki/Schedule---Google-Sheets-Integration
 class Schedule
   def initialize(spreadsheet_id, sheet = 0)
-    cache_key = "schedule/#{spreadsheet_id}"
-    fresh_data = false
-    parsed_response = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
-      fresh_data = true
-      response = HTTParty.get(SHEETS_URL + "#{spreadsheet_id}?#{SHEETS_FIELDS}&key=#{SHEETS_KEY}")
-      response ? response.parsed_response.to_hash : nil
-    end
+    @spreadsheet_id = spreadsheet_id
 
-    unless parsed_response && parsed_response["error"].nil?
-      error_message = "Error reading Google Sheet #{spreadsheet_id}. Response: #{parsed_response['error'].inspect}"
-      Rails.logger.error error_message
-      Rollbar.error(error_message) if fresh_data # Prevent spam of Rollbar pings
+    if !sheet_data || sheet_data["error"].present?
+      Rails.logger.error "Error reading Google Sheet #{spreadsheet_id}. Response: #{sheet_data['error'].inspect}"
       return
     end
 
-    @sheet = parsed_response["sheets"][sheet]
+    @sheet = sheet_data["sheets"][sheet]
   end
 
   def rows
-    rows = @sheet ? @sheet["data"][0]["rowData"] : []
-    # The first row is an instructional row
-    rows.count > 1 ? rows.drop(1) : []
+    @sheet ? @sheet["data"][0]["rowData"] : []
   end
 
   def sections
@@ -45,6 +34,21 @@ class Schedule
     end
     sections << section unless section.empty?
     sections
+  end
+
+  def sheet_data
+    # There's two levels of caching going on here.
+    # 1. "@sheet_data ||=" which caches in memory for subsequent calls to sheet_data() within the same Schedule instance / web request
+    # 2. "Rails.cache" which caches between requests (up until expiry)
+    @sheet_data ||= Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
+      response = HTTParty.get("#{SHEETS_URL}/#{@spreadsheet_id}?#{SHEETS_FIELDS}&key=#{SHEETS_KEY}")
+      # to_hash is used here to guarantee Rails/Redis can cache it properly
+      response ? response.parsed_response.to_hash : nil
+    end
+  end
+
+  def cache_key
+    "schedule/#{@spreadsheet_id}"
   end
 
   private
