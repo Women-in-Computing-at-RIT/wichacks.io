4f7332d822a0b2d85fc6da2bc2cfc136d7072a6c
amy <amytan265@gmail.com>
2021-01-18 15:47:20 -0500

    deleted old files

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-Rails.application.routes.draw do
-  get '/splash', to: 'pages#splash'
-  get '/extras', to: 'pages#extras'
-  get '/home', to: 'pages#home'
-
-  root 'pages#home'
-end
--- wichacks.io/.travis/deploy_stage.sh
+++
@@ -1,6 +0,0 @@
-eval "$(ssh-agent -s)" # start the ssh agent
-openssl aes-256-cbc -K $encrypted_493a21d0641d_key -iv $encrypted_493a21d0641d_iv -in .travis/deploy_ssh_key.enc -out .travis/deploy_ssh_key -d
-chmod 600 .travis/deploy_ssh_key # this key should have push access
-ssh-add .travis/deploy_ssh_key
-git remote add deploy dokku@wic.oweb.co:wichacks-stage
-git push --force deploy develop:master
